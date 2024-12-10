f88944dc829b4685cce625f6fad9a4d95040e2a3
sebastian2796 <sebastian2796@yahoo.com>
2024-12-10 13:51:48 -0500

    tutor work

--- Starter_Code_python_api/Starter_Code/api_keys.py
+++ Starter_Code_python_api/Starter_Code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "0f5508988eb11b81101b58110e694bbb"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "fa066082fa7740b89d3de209b8b8e624"
